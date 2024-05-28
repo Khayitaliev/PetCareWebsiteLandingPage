fa96dd7a610cc13f109fc22c55a27d56943da884
Bekhruz <117586415+Khayitaliev@users.noreply.github.com>
2024-05-28 20:12:59 +0500

    push

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-const burgerBtn = document.querySelector(".burger-btn");
-console.log(burgerBtn)
+const menu = document.querySelector(".nav");
+const burgerBtn = document.querySelector(".burger-btn")
+burgerBtn.addEventListener('click', function () {
+    menu.classList.toggle("active__nav")
+})
+
+
+// .1 let list = document.querySelector".list")
+// 2 let burgerBtn = document.querySelector(".burger");
+// 4 burgerBtn.addEventListener("click", function () {
+//     list.classList.toggle("active_navbar")
