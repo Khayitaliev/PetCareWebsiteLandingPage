f4f7647648862d9d958a11ec8654e04e885bf4aa
Bekhruz <117586415+Khayitaliev@users.noreply.github.com>
2024-07-22 14:02:34 +0500

    update

--- js/main.js
+++ js/main.js
@@ -4,8 +4,3 @@ burgerBtn.addEventListener('click', function () {
     menu.classList.toggle("active__nav")
 })
 
-
-// .1 let list = document.querySelector".list")
-// 2 let burgerBtn = document.querySelector(".burger");
-// 4 burgerBtn.addEventListener("click", function () {
-//     list.classList.toggle("active_navbar")
